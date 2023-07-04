66157088a4a14491479126475f6447ca69a43aa2
onofre <brunoonofre64@gmail.com>
2023-07-04 19:23:44 -0300

    Initial commit

---
+++ src/test/java/io/github/brunoonofre64/jspproject/JspProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brunoonofre64.jspproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
